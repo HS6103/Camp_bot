16213ea5269aaecfe489a642da52a0e27458c917
MSI\user <hsuchen1022@gmail.com>
2023-09-07 16:10:09 +0800

    modified account input

--- CampBot/five_nine_grade.py
+++ CampBot/five_nine_grade.py
@@ -95,7 +95,7 @@ except:
 LOKI_URL = "https://api.droidtown.co/Loki/BulkAPI/"
 try:
     accountInfo = json.load(open(os.path.join(os.path.dirname(__file__), "account.info"), encoding="utf-8"))
-    USERNAME = accountInfo["username_59"]
+    USERNAME = accountInfo["username"]
     LOKI_KEY = accountInfo["loki_key_fivenine"]
 except Exception as e:
     print("[ERROR] AccountInfo => {}".format(str(e)))
